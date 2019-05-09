ad632148827b646ebd63854072d755d51ed29241
Samuel Défago <defagos@gmail.com>
2019-05-09 22:06:12 +0200

    Add bridge for animated reloads

---
+++ Application/Sources/Bridges/DeepDiffBridge.swift
@@ -0,0 +1,44 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import DeepDiff
+import UIKit
+
+extension NSObject: DiffAware {
+    public var diffId: Int {
+        return self.hashValue
+    }
+    
+    public static func compareContent(_ a: NSObject, _ b: NSObject) -> Bool {
+        return a == b
+    }
+}
+
+extension UITableView {
+    @objc public func reloadDataAnimated(oldObjects: [NSObject], newObjects: [NSObject], section: Int = 0, updateData: () -> Void) {
+        if !oldObjects.isEmpty && !newObjects.isEmpty {
+            let changes = diff(old: oldObjects, new: newObjects)
+            self.reload(changes: changes, section: section, insertionAnimation: .automatic, deletionAnimation: .automatic, replacementAnimation: .automatic, updateData: updateData, completion: nil)
+        }
+        else {
+            updateData()
+            self.reloadData()
+        }
+    }
+}
+
+extension UICollectionView {
+    @objc public func reloadDataAnimated(oldObjects: [NSObject], newObjects: [NSObject], updateData: () -> Void) {
+        if !oldObjects.isEmpty && !newObjects.isEmpty {
+            let changes = diff(old: oldObjects, new: newObjects)
+            self.reload(changes: changes, updateData: updateData)
+        }
+        else {
+            updateData()
+            self.reloadData()
+        }
+    }
+}
